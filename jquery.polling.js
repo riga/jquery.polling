c216101d0df59193fa1ab0688a8f0831af53ee59
Riga <marcelrieger@me.com>
2013-01-07 09:19:50 +0100

    Fix empty parameter bug in 'lock'.

--- jquery.polling.js
+++ jquery.polling.js
@@ -10,185 +10,186 @@
 
 var _polling = {};
 
-jQuery.Polling = function( id ) {
-	
-	var self = id && _polling[ id ];
-	
-	if ( !self ) {
-		// callbacks store
-		var _callbacks = [],
-		
-		// notification store
-		_notifications = jQuery.Callbacks(),
-		
-		// default config
-		_config = {
-			autoRun: false,
-			delay: 5000,
-			cycles: -1,
-			end: jQuery.Callbacks()
-		},
-		
-		// run and lock vars
-		_counter = 0,
-		_isRunning = false,
-		_timeout = null,
-		_isLocked = false,
-		_doLock = false,
-		_lockDeferred = jQuery.Deferred(),
-		
-		// setup function
-		setup = function( __config ) {
-			jQuery.extend( true, _config, __config );
-			return this;
-		},
-		
-		// add function
-		add = function() {
-			var args = jQuery.makeArray( arguments );
-			if ( args.length ) {
-				_callbacks = jQuery.merge( _callbacks, args );
-				// autorun?
-				if ( _config.autoRun ) {
-					run();
-				}
-			}
-			return this;
-		},
-		
-		// run function
-		run = function() {
-			window.clearTimeout( _timeout );
-			if (!_isLocked) {
-				_isRunning = true;
-				var promises = [];
-				jQuery.each( _callbacks, function( i, callback ) {
-					var result = callback();
-					promises.push( (result && result.promise) ? result.promise() : result );
-				});
-				
-				_counter++;
-				if ( _config.cycles > -1 && _counter >= _config.cycles ) {
-					lock( _config.end.fire || _config.end );
-				}
-				
-				jQuery.when.apply( null, promises ).then(function() {
-					_notifications.fire.apply( _notifications, promises );
-					if ( _doLock ) {
-						_lockDeferred.resolve();
-					} else {
-						_timeout = window.setTimeout( run, _config.delay );
-					}
-				});
-			}
-			return this;
-		},
-		
-		// remove function
-		remove = function() {
-			var args = jQuery.makeArray( arguments );
-			var dfd = jQuery.Deferred().done(function () {
-				_callbacks = jQuery.grep( _callbacks, function( callback ) {
-					return jQuery.inArray( callback, args ) == -1;
-				});
-				unlock();
-			});
-			lock( dfd.resolve );
-			return this;
-		},
-		
-		// empty function
-		empty = function() {
-			var dfd = jQuery.Deferred().done(function() {
-				_callbacks = [];
-				unlock();
-			});
-			lock( dfd.resolve );
-			return this;
-		},
-		
-		// lock function
-		lock = function( callback ) {
-			callback = callback.fire || callback;
-			if ( !_isRunning || _timeout ) {
-				window.clearTimeout( _timeout );
-				_isRunning = false;
-				_isLocked = true;
-				callback();
-			} else {
-				_lockDeferred = jQuery.Deferred().done(function() {
-					_isLocked = true;
-					_doLock = _isRunning = false;
-				}, callback);
-				_doLock = true;
-			}
-			return this;
-		},
-		
-		// onlock functiou
-		unlock = function( resetCounter ) {
-			_counter = resetCounter ? 0 : _counter;
-			_doLock = false;
-			if ( _isLocked ) {
-				_isLocked = false;
-				run();
-			}
-			return this;
-		},
-		
-		// notify function
-		notify = function() {
-			_notifications.add( jQuery.makeArray( arguments ) );
-			return this;
-		},
-		
-		// disnotify function
-		disnotify = function() {
-			_notifications.remove( jQuery.makeArray( arguments ) );
-			return this;
-		},
-		
-		// emptyNotify function
-		emptyNotify = function() {
-			_notifications.empty();
-			return this;
-		},
-		
-		// _counter getter
-		count = function() {
-			return _counter;
-		},
-		
-		// _isLocked getter
-		locked = function() {
-			return _isLocked;
-		},
-		
-		// _isRunning getter
-		running = function() {
-			return _isRunning;
-		};
-		
-		self = {
-			setup: setup,
-			add: add,
-			run: run,
-			remove: remove,
-			empty: empty,
-			lock: lock,
-			unlock: unlock,
-			notify: notify,
-			disnotify: disnotify,
-			emptyNotify: emptyNotify,
-			count: count,
-			locked: locked,
-			running: running
-		};
-		
-		if ( id ) {
-			_polling[ id ] = self;
-		}
-	}
-	
-	return self;
+jQuery.Polling = function(id) {
+
+    var self = id && _polling[id];
+
+    if(!self) {
+        // callbacks store
+        var _callbacks = [],
+
+        // notification store
+        _notifications = jQuery.Callbacks(),
+
+        // default config
+        _config = {
+            autoRun: false,
+            delay: 5000,
+            cycles: -1,
+            end: jQuery.Callbacks()
+        },
+
+        // run and lock vars
+        _counter = 0,
+        _isRunning = false,
+        _timeout = null,
+        _isLocked = false,
+        _doLock = false,
+        _lockDeferred = jQuery.Deferred(),
+
+        // setup function
+        setup = function(__config) {
+            jQuery.extend(true, _config, __config);
+            return this;
+        },
+
+        // add function
+        add = function() {
+            var args = jQuery.makeArray(arguments);
+            if(args.length) {
+                _callbacks = jQuery.merge(_callbacks, args);
+                // autorun?
+                if(_config.autoRun) {
+                    run();
+                }
+            }
+            return this;
+        },
+
+        // run function
+        run = function() {
+            window.clearTimeout(_timeout);
+            if(!_isLocked) {
+                _isRunning = true;
+                var promises = [];
+                jQuery.each(_callbacks, function(i, callback) {
+                    var result = callback();
+                    promises.push((result && result.promise) ? result.promise() : result);
+                });
+
+                _counter++;
+                if(_config.cycles > -1 && _counter >= _config.cycles) {
+                    lock(_config.end.fire || _config.end);
+                }
+
+                jQuery.when.apply(null, promises).then(function() {
+                    _notifications.fire.apply(_notifications, promises);
+                    if(_doLock) {
+                        _lockDeferred.resolve();
+                    } else {
+                        _timeout = window.setTimeout(run, _config.delay);
+                    }
+                });
+            }
+            return this;
+        },
+
+        // remove function
+        remove = function() {
+            var args = jQuery.makeArray(arguments);
+            var dfd = jQuery.Deferred().done(function() {
+                _callbacks = jQuery.grep(_callbacks, function(callback) {
+                    return jQuery.inArray(callback, args) == -1;
+                });
+                unlock();
+            });
+            lock(dfd.resolve);
+            return this;
+        },
+
+        // empty function
+        empty = function() {
+            var dfd = jQuery.Deferred().done(function() {
+                _callbacks = [];
+                unlock();
+            });
+            lock(dfd.resolve);
+            return this;
+        },
+
+        // lock function
+        lock = function(callback) {
+            callback = callback || function(){};
+            callback = callback.fire || callback;
+            if(!_isRunning || _timeout) {
+                window.clearTimeout(_timeout);
+                _isRunning = false;
+                _isLocked = true;
+                callback();
+            } else {
+                _lockDeferred = jQuery.Deferred().done(function() {
+                    _isLocked = true;
+                    _doLock = _isRunning = false;
+                }, callback);
+                _doLock = true;
+            }
+            return this;
+        },
+
+        // onlock functiou
+        unlock = function(resetCounter) {
+            _counter = resetCounter ? 0 : _counter;
+            _doLock = false;
+            if(_isLocked) {
+                _isLocked = false;
+                run();
+            }
+            return this;
+        },
+
+        // notify function
+        notify = function() {
+            _notifications.add(jQuery.makeArray(arguments));
+            return this;
+        },
+
+        // disnotify function
+        disnotify = function() {
+            _notifications.remove(jQuery.makeArray(arguments));
+            return this;
+        },
+
+        // emptyNotify function
+        emptyNotify = function() {
+            _notifications.empty();
+            return this;
+        },
+
+        // _counter getter
+        count = function() {
+            return _counter;
+        },
+
+        // _isLocked getter
+        locked = function() {
+            return _isLocked;
+        },
+
+        // _isRunning getter
+        running = function() {
+            return _isRunning;
+        };
+
+        self = {
+            setup: setup,
+            add: add,
+            run: run,
+            remove: remove,
+            empty: empty,
+            lock: lock,
+            unlock: unlock,
+            notify: notify,
+            disnotify: disnotify,
+            emptyNotify: emptyNotify,
+            count: count,
+            locked: locked,
+            running: running
+        };
+
+        if(id) {
+            _polling[id] = self;
+        }
+    }
+
+    return self;
 };
